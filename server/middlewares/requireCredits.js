3d4987f4f38ea744c2f7ce29874b530468b621b7
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-02-06 15:25:31 -0500

    commit

--- server/middlewares/requireCredits.js
+++ server/middlewares/requireCredits.js
@@ -1,4 +1,4 @@
-//monitor the credit if credit <1 return
+//monitor the credit if credit <1 return 
 module.exports = (req, res, next) => {
   if (req.user.credits<1) {
     return res.status(403).send({ error: "Not enough credits!" });
